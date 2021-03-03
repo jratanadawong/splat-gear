41e153900596347eb153771ca535d544b8514040
JordPWN <jratanadawong@500px.com>
2021-03-03 10:28:32 -0500

    Add data constant helpers, and create utlity to manipulate JSON data

---
+++ src/utils/constants.js
@@ -0,0 +1,53 @@
+const FILE_DICT = {
+  shoes: "GearInfo_Shoes.json",
+  clothes: "GearInfo_Clothes.json",
+  head: "GearInfo_Head.json",
+  sub: "WeaponInfo_Sub.json",
+  special: "WeaponInfo_Special.json",
+  main: "WeaponInfo_Main.json",
+};
+
+const DIRECTORY_DICT = {
+  shoes: "gear",
+  head: "gear",
+  clothes: "gear",
+  skill: "skills",
+  sub: "subspe",
+  special: "subspe",
+  weapon: "weapons",
+  brand: "brands",
+};
+
+const ASSET_PREFIX = {
+  weapon: "Wst_",
+  sub: "Wsb_",
+  special: "Wsp_",
+};
+
+const SKILL_DICT = {
+  bdx: "BombDamage_Reduction",
+  bpu: "BombDistance_Up",
+  comeback: "ComeBack",
+  haunt: "DeathMarking",
+  lde: "EndAllUp",
+  rsu: "HumanMove_Up",
+  inkRec: "InkRecovery_Up",
+  qsj: "JumpTime_Save",
+  ism: "MainInk_Save",
+  mpu: "MarkingTime_Reduction",
+  ten: "MinorityUp",
+  os: "ObjectEffect_Up",
+  inkRes: "OpInkEffect_Reduction",
+  ss: "RespawnSpecialGuage_Save",
+  qr: "RespawnTime_Save",
+  dr: "SomersaultLanding",
+  scu: "SpecialIncrease_Up",
+  spu: "SpecialTime_Up",
+  ssu: "SquidMove_Up",
+  ns: "SquidMoveSpatter_Reduction",
+  og: "StartAllUp",
+  iss: "SubInk_Save",
+  sj: "SuperJumpSign_Hide",
+  ti: "ThermalInk",
+  unknown: "Unknown",
+};
